577a0f49bf3c1b5989ff7faa72d7cd862246df7a
raisur <raisur.rahman234@gmail.com>
2018-12-04 03:35:11 +0000

    third commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+  var server = http.createServer(function(req, res){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end();
+  });
+  server.listen(process.env.PORT, process.env.IP, function(){
+    console.log('Server running');
+  });
+  
+ 
